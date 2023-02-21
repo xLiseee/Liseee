2223799687f7be352a45963f368ec311d5a0fa2c
liseversmissenpxl <lise.versmissen@student.pxl.be>
2023-02-21 21:20:47 +0100

    21/02

--- script.js
+++ script.js
@@ -3,20 +3,32 @@ const statusMessage = document.querySelector('#status-message');
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
-
-    const formData = new FormData(form);
-    const response = await fetch(form.action, {
-        method: form.method,
-        body: formData,
+const name = document.getElementById("name")
+    const email = document.getElementById("email")
+    const message = document.getElementById("message")
+    console.log(name.value,email.value,message.value)
+    await fetch('https://api.useplunk.com/v1/track', {
+        method: "POST",
         headers: {
-            'Accept': 'application/json'
-        }
+            "Content-Type": "application/json",
+            "Authorization": "Bearer pk_4c039fde95b0ab2e15b214e105eeb812e51a0375d0a1f6f4",
+        },
+        body: JSON.stringify({
+            "event": "contact form submitted",
+            "email": email.value,
+            'data': {'name':name.value, 'message':message.value}
+        }),
     });
-
-    if (response.ok) {
-        form.reset();
-        statusMessage.textContent = 'Thanks for contacting us!';
-    } else {
-        statusMessage.textContent = 'Oops! Something went wrong.';
-    }
+    await fetch('https://api.useplunk.com/v1/track', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer pk_4c039fde95b0ab2e15b214e105eeb812e51a0375d0a1f6f4",
+        },
+        body: JSON.stringify({
+            "event": "contact form submitted",
+            "email": 'lv-graphics@outlook.com',
+            'data': {'name':name.value, 'message':message.value}
+        }),
+    });
 });
